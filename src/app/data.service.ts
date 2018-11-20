caa2b7bc2e7a87092ac480aefffd1ebe1ec02686
5636 <5636@ait-sydney.net.au>
2018-11-20 15:50:53 +1100

    added data service

---
+++ src/app/data.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+  //method to store dataService
+  storeList( list:Array<string>){
+    return new Promise( (resolve,reject)=>{
+      let data = JSON.stringify( list );
+      try {
+        window.localStorage.setItem('list', data);
+        if (window.localStorage.getItem('list')){
+          resolve(true);
+        }
+        else{
+          throw('save failed');
+        }
+      }
+  catch( error){
+    reject( error);
+  }
+    });
+  }
+
+  //method to read data
+  loadList(){
+    return new Promise( (resolve, reject)=>{
+      let data = window.localStorage.getItem('list');
+      if( data ){
+        resolve( JSON.parse(data) );
+
+      }
+      else{
+        reject( null );
+      }
+    });
+  }
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,44 @@
 import { Component } from '@angular/core';
+import {DataService} from '../data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+  title:string ='Todo List';
+  fruits:Array<string> = [];
+  taskInput : string ='';
+  listTitle = 'Todo List';
+  constructor(public dataService: DataService){
+
+  }
+
+
+addTask(){
+  if (this.taskInput.length>0){
+    this.fruits.push(this.taskInput);
+    this.taskInput ='';
+    this.dataService.storeList(this.fruits)//save data
+    .then( (response ) => {
+      //sucess
+    })
+    .catch( (error) => {
+      console.log( error);
+    });
+  }
+}
+
+readTasks(){
+  this.dataService.loadList()
+  .then( ( response ) => {
+    if (response !== null){
+      this.fruits = <Array<string>>response;
+    }
+  })
+  .catch( ( error ) => {
+    console.log(error);
+  })
+}
+}
